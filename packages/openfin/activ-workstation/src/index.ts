549d919589080820ffbb877388fac128929acf29
Luke Elliott <luke.elliott@activfinancial.com>
2019-09-04 18:30:08 +0100

    refactor(activ-workstation): example imports don't need nested path again.

--- packages/openfin/activ-workstation/src/index.ts
+++ packages/openfin/activ-workstation/src/index.ts
@@ -15,13 +15,10 @@ import {
     asyncSleep
 } from "@activfinancial/cg-api";
 
-// NB examples classes are exported from top level (e.g. @activfinancial/record-viewer)
-// but there seems to be some issue with lit-element and decorators; can't see it extends
-// HTMLElement for some reason (or maybe it's an issue with dts-generator). So use full path.
-import OptionChain from "@activfinancial/option-chain/src/index";
-import NewsViewer from "@activfinancial/news-viewer/src/index";
-import Chart from "@activfinancial/time-series-chart/src/index";
-import RecordViewer from "@activfinancial/record-viewer/src/index";
+import OptionChain from "@activfinancial/option-chain";
+import NewsViewer from "@activfinancial/news-viewer";
+import Chart from "@activfinancial/time-series-chart";
+import RecordViewer from "@activfinancial/record-viewer";
 
 import symbolTableRow from "!html-loader!./symbolTableRow.html";
 
