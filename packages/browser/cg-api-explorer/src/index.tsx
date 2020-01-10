15c634b2f116ebe7899ca42ad1297282eb599ede
Luke Elliott <luke.elliott@activfinancial.com>
2020-01-10 15:20:15 +0000

    chore(cg-api-explorer): removed why-did-you-render dependency as unused.

--- packages/browser/cg-api-explorer/src/index.tsx
+++ packages/browser/cg-api-explorer/src/index.tsx
@@ -9,9 +9,6 @@ import App from "./App";
 import "index.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-// const whyDidYouRender = require("@welldone-software/why-did-you-render/dist/no-classes-transpile/umd/whyDidYouRender.min.js");
-// whyDidYouRender(React, { logOnDifferentValues: true });
-
 // ---------------------------------------------------------------------------------------------------------------------------------
 
 ReactDOM.render(<App />, document.getElementById("root"));
